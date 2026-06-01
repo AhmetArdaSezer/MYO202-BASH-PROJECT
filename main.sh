9e06bc75c080ebb4fc6594567de744aaea0a6793
ardasezer <ardasezer.253@gmail.com>
2026-06-01 18:49:46 +0300

    Sertifika linklerindeki parantezler duzeltildi

--- main.sh
+++ main.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# İsim SOYİSİM: Ahmet Arda Sezer
+# İsim Soyisim: Ahmet Arda Sezer
 # Öğrenci Numarası: 2420191025
-# Sertifika Bağlantıları: 
-# 1. Docker: https://drive.google.com/file/d/1wgMW-bHv9dvwoFTA5lk7riesY1-uOaLn/view?usp=drive_link
-# 2. Linux: https://drive.google.com/file/d/14FvEB0Y0TdUchwrUwp1q30SN6HOC1A_X/view?usp=drive_link
-# 3. Bash Script:https://credsverse.com/credentials/3e26ce31-a90e-4dc0-a8ed-32ca36ef823f 
+# Sertifika Bağlantıları:
+# 1. Docker: https://www.btkakademi.gov.tr/portal/certificate/validate?certificateId=AJaS7OWLnE
+# 2. Linux: https://www.btkakademi.gov.tr/portal/certificate/validate?certificateId=jK1hGb6LoL
+# 3. Bash Script: https://credsverse.com/credentials/3e26ce31-a90e-4dc0-a8ed-32ca36ef823f
 
 echo "Başlangıç Zamanı: $(date -Iseconds)" > report.log
 echo -e "\n--- DONANIM BİLGİLERİ ---" >> report.log
@@ -17,8 +17,10 @@ echo -e "\n[RAM]" >> report.log
 wmic memorychip get capacity >> report.log 2>/dev/null
 echo -e "\n[Anakart]" >> report.log
 wmic baseboard get product,Manufacturer >> report.log 2>/dev/null
-echo -e "\n[UUID]" >> report.log
+echo -e "\n[Anakart UUID]" >> report.log
 wmic csproduct get uuid >> report.log 2>/dev/null
+echo -e "\n[Disk Bilgisi]" >> report.log
+wmic diskdrive get model,mediatype >> report.log 2>/dev/null
 echo -e "\n[MAC Adresi]" >> report.log
 getmac >> report.log 2>/dev/null
 
